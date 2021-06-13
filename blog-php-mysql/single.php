f061759db8f35306339f4375f9bec88031a69076
Do Binh Duy <53504730+dobinhduy@users.noreply.github.com>
2021-06-13 18:59:41 +0700

    contact+language

--- blog-php-mysql/single.php
+++ blog-php-mysql/single.php
@@ -129,33 +129,7 @@ $posts = selectAll('posts', ['published' => 1]);
 
   <!-- Custom Script -->
   <script src="assets/js/scripts.js"></script>
-  <script>
-    function googleTranslateElementInit() {
-    new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.FloatPosition.TOP_LEFT}, 'google_translate_element');
-   }
 
-   function triggerHtmlEvent(element, eventName) {
-    var event;
-    if (document.createEvent) {
-      event = document.createEvent('HTMLEvents');
-      event.initEvent(eventName, true, true);
-      element.dispatchEvent(event);
-    } else {
-      event = document.createEventObject();
-      event.eventType = eventName;
-      element.fireEvent('on' + event.eventType, event);
-    }
-   }
-
-   jQuery('.lang-select').click(function() {
-    var theLang = jQuery(this).attr('data-lang');
-    jQuery('.goog-te-combo').val(theLang);
-
-    window.location = jQuery(this).attr('href');
-    location.reload();
-
-   });
-  </script>
 
 </body>
 
